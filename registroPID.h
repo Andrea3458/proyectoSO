31c59fc70155c648f81d69c335ee8fc48406236d
Andrea3458 <carolina3458.x@gmail.com>
2025-06-22 19:04:27 -0400

    registroPID

--- registroPID.h
+++ registroPID.h
@@ -1,27 +1,54 @@
 #ifndef REGISTRO_PID_H  // Guardia de inclusión para evitar problemas de múltiples inclusiones
 #define REGISTRO_PID_H
 
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
-void registrarPID(pid_t padre, bool *esPrimeraIteracion, int *i){
+void registrarPID(pid_t pid);
+
+#endif
+
+#include <cstdlib>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+static pid_t pid_padre = 0;
+static int intervalo = 0;
+
+void registrarPID(pid_t pid){
+
+    // Verificar si es la primera llamada (proceso padre)
+     if (pid_padre == 0) {
+        pid_padre = getpid();
+    }
+
     FILE* archivo;
 
     archivo = fopen("30371074_31532714_31462340.txt","a+");
 
     if(archivo == NULL){
-        printf("Hubo un problema, no se pudo crear archivo\n");
-        return;
+        perror("Error al abrir archivo de registro");
+        exit(EXIT_FAILURE);
     }
 
-    if(esPrimeraIteracion){
-        fprintf(archivo, "PID Padre: %d\n", getpid());
-        esPrimeraIteracion = false;
-    } else {
-        fprintf(archivo, "PID Hijo: %d\n", padre); /*Modificar para N Cantidad de Hijos*/
+    // Escribir PID del padre si el tamaño del archivo es 0 (esta vacio)
+    fseek(archivo, 0, SEEK_END);
+    if (ftell(archivo) == 0) {
+        fprintf(archivo, "PID Padre: %d\n", pid_padre);
     }
-    fprintf(archivo, "Intervalo %d\n",i);
 
-    i++;
+    fflush(archivo); // controlar buffer de escritura
+
+    if (pid_padre == 0) {
+        // Es un nuevo intervalo (llamada desde el padre)
+        fprintf(archivo, "\nIntervalo %d\n", intervalo++);
+    } else if (pid > 0 && getpid() == pid_padre) {
+        fprintf(archivo, "PID Hijo: %d\n", pid);
+    }
+    
+    fflush(archivo);
+
     fclose(archivo);
 }
