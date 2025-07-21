95de86a9e72d95dd1ec6a0d37997b6be764d8d70
Andrea3458 <carolina3458.x@gmail.com>
2025-07-21 15:45:02 -0400

    cola y struct de procesos

---
+++ parte2/cola.h
@@ -0,0 +1,23 @@
+// queue.h
+#ifndef COLA_H
+#define COLA_H
+
+#include "procesos.h"
+
+typedef struct {
+    Proceso *procesos;
+    int capacidad_max;
+    int tamano_actual;
+    int inicio;
+    int final;
+} Cola;
+
+// Prototipos de funciones
+Cola* crear_Cola(int capacidad);
+void destruir_Cola(Cola *cola);
+int is_empty(Cola *cola);
+int is_full(Cola *cola);
+void agregar_proceso(Cola *cola, Proceso p);
+Proceso eliminar_proceso(Cola *cola);
+
+#endif
--- parte2/procesos.h
+++ parte2/procesos.h
@@ -5,7 +5,17 @@
 #include <unistd.h>
 #include <time.h>
 
+typedef struct {
+    int tiempo_llegada;
+    int prioridad;
+    int tiempo_procesador; 
+    int num_impresoras;
+    int num_scanners;
+    int num_modems;
+    int num_DVDs;
+} Proceso;
+
 void crear_procesos();
-void registrar_mensajes();
+void registrar_mensajes(int segundo, int id_proceso, const char *estado);
 
 #endif
