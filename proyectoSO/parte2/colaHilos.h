bdcae2431a58e030324512f15f9e12344a379a00
JuanJoSam <juanbeliuni@gmail.com>
2025-07-23 16:13:03 -0400

    v2

--- proyectoSO/parte2/colaHilos.h
+++ proyectoSO/parte2/colaHilos.h
@@ -11,4 +11,12 @@ typedef struct {
     int final;
 } ColaHilos;
 
+// Prototipos de funciones
+ColaHilos* crear_Cola_Hilos(int capacidad);
+void destruir_Cola_Hilos(ColaHilos *cola);
+int is_emptyH(ColaHilos *cola);
+int is_fullH(ColaHilos *cola);
+void agregar_hilo(ColaHilos *cola, pthread_t p);
+pthread_t eliminar_hilo(ColaHilos *cola);
+
 typedef 
