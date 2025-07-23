ecca1f2831d4acd5195b882fa4dcc3f886168e49
JuanJoSam <juanbeliuni@gmail.com>
2025-07-23 18:04:00 -0400

    fix4

--- proyectoSO/parte2/despachador.c
+++ proyectoSO/parte2/despachador.c
@@ -56,9 +56,9 @@ int main (int argc, char *argv[]) {
 
             //Asignar procesos a la cola correspondiente
             if(proc_temp.prioridad == 0){
-                agregar_proceso(&tiempo_real, lista_procesos);
+                agregar_proceso(&tiempo_real, proc_temp);
             } else {
-                agregar_proceso(&usuario, lista_procesos);
+                agregar_proceso(&usuario, proc_temp);
             }
 
             //Crear Proceso || Contador en tiempo del proceso en SO
