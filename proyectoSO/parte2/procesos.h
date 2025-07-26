0d29e6d437f215127131515e288e9a33c74267a2
JuanJoSam <juanbeliuni@gmail.com>
2025-07-26 18:32:22 -0400

    fix4

--- proyectoSO/parte2/procesos.h
+++ proyectoSO/parte2/procesos.h
@@ -3,7 +3,6 @@
 
 #include <pthread.h>
 #include <semaphore.h>
-#include "cola.h"
 
 extern int segundo_actual, id_actual, cont_hilos_ejecucion, max_hilos_ejecucion, hay_proceso_en_ejecucion;
 extern sem_t sem_ejecucion, sem_hilos_terminaron, sem_mutex;
