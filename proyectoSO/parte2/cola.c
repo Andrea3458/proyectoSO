85b6e74a84c4e2493368d24fe372dc4b8a6f2ae5
JuanJoSam <juanbeliuni@gmail.com>
2025-07-27 01:46:13 -0400

    fix30

--- proyectoSO/parte2/cola.c
+++ proyectoSO/parte2/cola.c
@@ -71,6 +71,7 @@ int esta_proceso_en_la_cola(Cola *q, Proceso p){
         Proceso auxP = eliminar_proceso(&auxQ);
 
         if(p.id == auxP.id){
+            printf("ID: %d\n",p.id);
             return 1;
         }
 
