2dd073b99703a5b802822224aec4adf6c790c14c
JuanJoSam <juanbeliuni@gmail.com>
2025-07-27 00:04:40 -0400

    fix21

--- proyectoSO/parte2/despachador.c
+++ proyectoSO/parte2/despachador.c
@@ -118,6 +118,7 @@ int main (int argc, char *argv[]) {
             //ASIGNAR PROCESO A SU COLA DE PRIORIDAD
             while(!is_empty(&usuario)){
 
+                printf("%d\n",adquirir_recursos(&proc));
                 if(adquirir_recursos(&proc)) {
                 
                 proc = eliminar_proceso(&usuario);
