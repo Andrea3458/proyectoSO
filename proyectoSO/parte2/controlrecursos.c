4789f234d7800819f0f60100a8d75cbb896f14a8
JuanJoSam <juanbeliuni@gmail.com>
2025-07-27 01:34:25 -0400

    fix30

--- proyectoSO/parte2/controlrecursos.c
+++ proyectoSO/parte2/controlrecursos.c
@@ -7,24 +7,16 @@ int adquirir_recursos(Proceso* proc) {
         return 0;
     }*/
     // Intentar adquirir impresoras
-    for(int i = 0; i < proc->num_impresoras; i++) {
-        numImpresoras--;
-    }
+    num_impresoras -= proc->num_impresoras;
     
     // Intentar adquirir scanner
-    if(proc->num_scanners > 0) {
-        numScanner--;
-    }
+    numScanner -= proc->num_scanners;
     
     // Intentar adquirir modem
-    if(proc->num_modems > 0) {
-        numModem--;
-    }
+    numModem -= proc->num_modems;
     
     // Intentar adquirir lectores DVD
-    for(int i = 0; i < proc->num_DVDs; i++) {
-        numLectoresDVD--;
-    }
+    numLectoresDVD -= proc->num_DVDs;
     
     return numImpresoras >= 0 && numScanner >= 0 && numModem >= 0 && numLectoresDVD >= 0;
 }
@@ -33,22 +25,14 @@ int adquirir_recursos(Proceso* proc) {
 void liberar_recursos(Proceso* proc) {
     
     // Liberar impresoras
-    for(int i = 0; i < proc->num_impresoras; i++) {
-        numImpresoras++;
-    }
+    num_impresoras += proc->num_impresoras;
     
     // Intentar adquirir scanner
-    if(proc->num_scanners > 0) {
-        numScanner++;
-    }
+    numScanner += proc->num_scanners;
     
     // Intentar adquirir modem
-    if(proc->num_modems > 0) {
-        numModem++;
-    }
+    numModem += proc->num_modems;
     
     // Intentar adquirir lectores DVD
-    for(int i = 0; i < proc->num_DVDs; i++) {
-        numLectoresDVD++;
-    }
+    numLectoresDVD += proc->num_DVDs;
 }
