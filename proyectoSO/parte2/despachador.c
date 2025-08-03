bd3041079fdcc38aa386ae0332a4ddb3f56d9dc7
JuanJoSam <juanbeliuni@gmail.com>
2025-08-03 08:55:02 -0400

    fix3

--- proyectoSO/parte2/despachador.c
+++ proyectoSO/parte2/despachador.c
@@ -218,7 +218,7 @@ int main (int argc, char *argv[]) {
             quantum--;
         }
         seleccionado_antes = 0;
-        printf("Cantidad: %d, HAY: %d\n",prioridad[2].tamano_actual, hay_proceso_en_ejecucion);
+        printf("Cantidad: %d, HAY: %d, id: %d\n",prioridad[2].tamano_actual, hay_proceso_en_ejecucion, id_actual);
 
         //Lógica semáforos
         control_semaforos();
